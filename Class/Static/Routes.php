d7895f121334286341f5b0f215a890576e534f45
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-18 22:28:10 -0300

    Creamos el archivo Routes.php, Declaramos la clase Routes, definimos su atributo estatico routes de tipo array, declaramos sus metodos estaticos get, post, put y delete todos con 2 parametros string y array

--- Class/Static/Routes.php
+++ Class/Static/Routes.php
@@ -1,2 +1,29 @@
 <?php
+namespace Class\Static;
+
+class Routes {
+
+    static array $routes = [
+        'GET' => [],
+        'POST' => [],
+        'PUT' => [],
+        'DELETE' => []
+    ];
+
+    public static function get(string $route, array $serviceClassAndMethod) {
+
+    }
+
+    public static function post(string $route, array $serviceClassAndMethod) {
+
+    }
+
+    public static function put(string $route, array $serviceClassAndMethod) {
+
+    }
+
+    public static function delete(string $route, array $serviceClassAndMethod) {
+
+    }
+}
 ?>
