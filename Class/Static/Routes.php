0d3daea0466db5e1c4bf3a7b2979eeafa1d7ccbf
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-18 23:43:31 -0300

    Definimos el metodo estatico privado addRoute usamos este metodo para definir los metodos estaticos publicos get, post, put, delete

--- Class/Static/Routes.php
+++ Class/Static/Routes.php
@@ -11,19 +11,24 @@ class Routes {
     ];
 
     public static function get(string $route, array $serviceClassAndMethod) {
-
+        self::addRoute('GET', $route, $serviceClassAndMethod);
     }
 
     public static function post(string $route, array $serviceClassAndMethod) {
-
+        self::addRoute('POST', $route, $serviceClassAndMethod);
     }
 
     public static function put(string $route, array $serviceClassAndMethod) {
-
+        self::addRoute('PUT', $route, $serviceClassAndMethod);
     }
 
     public static function delete(string $route, array $serviceClassAndMethod) {
+        self::addRoute('DELETE', $route, $serviceClassAndMethod);
+    }
 
+    private static function addRoute(string $httpMethod, string $route, array $serviceClassAndMethod) {
+        $arrayKeys = array('service', 'method');
+        array_push(self::$routes[strtoupper($httpMethod)], ['endPoint' => $route, 'serviceAndMethod' => array_combine($arrayKeys, $serviceClassAndMethod)]);
     }
 }
 ?>
