b01c5bd4fe5e7d4a4e6ba0d14539404294506f8f
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-20 17:05:48 -0300

    Creamos el archivo Json.php dentro de Class/Static, declaramos la clase Json tambien declaramos tres metodos estaticos publicos los cuales son convertirAFormatoJSON, convertirJsonAFormatoObject, convertirJsonAFormatoArrayAsociativo, los 3 con un parametro

---
+++ Class/Static/Json.php
@@ -0,0 +1,18 @@
+<?php
+namespace Class\Static;
+
+class Json {
+
+    public static function convertirAFormatoJson(array $bodyResponse) {
+
+    }
+
+    public static function convertirJsonAFormatoObject(string $dataJson) {
+
+    }
+
+    public static function convertirJsonAFormatoArrayAsociativo(string $dataJson) {
+
+    }
+}
+?>
