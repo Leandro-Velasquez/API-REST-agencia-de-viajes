b436bcdb468f990e731bbe10eb776478f87566ea
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-20 22:07:21 -0300

    Declaramos los metodos estaticos getUri, getService, getId, getHttpMethod, getBody y checkIfThereARequest de la clase Request

--- Class/Static/Request.php
+++ Class/Static/Request.php
@@ -2,5 +2,28 @@
 namespace Class\Static;
 
 class Request {
+    
+    public static function getUri() {
 
+    }
+
+    public static function getService() {
+
+    }
+
+    public static function getId() {
+
+    }
+
+    public static function getHttpMethod() {
+
+    }
+
+    public static function getBody() {
+
+    }
+
+    public static function checkIfThereARequest() {
+
+    }
 }
