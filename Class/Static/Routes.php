673a5efc525f687f97a81867a8a5d94886224205
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-20 22:37:31 -0300

    Documentamos los metodos de la clase Routes

--- Class/Static/Routes.php
+++ Class/Static/Routes.php
@@ -10,22 +10,58 @@ class Routes {
         'DELETE' => []
     ];
 
+    /**
+     * Agrega una ruta de la api junto a la clase y el metodo que van a administrar esa ruta
+     *
+     * @param string $route
+     * @param array $serviceClassAndMethod
+     * @return void
+     */
     public static function get(string $route, array $serviceClassAndMethod) {
         self::addRoute('GET', $route, $serviceClassAndMethod);
     }
 
+    /**
+     * Agrega una ruta de la api junto a la clase y el metodo que van a administrar esa ruta
+     *
+     * @param string $route
+     * @param array $serviceClassAndMethod
+     * @return void
+     */
     public static function post(string $route, array $serviceClassAndMethod) {
         self::addRoute('POST', $route, $serviceClassAndMethod);
     }
 
+    /**
+     * Agrega una ruta de la api junto a la clase y el metodo que van a administrar esa ruta
+     *
+     * @param string $route
+     * @param array $serviceClassAndMethod
+     * @return void
+     */
     public static function put(string $route, array $serviceClassAndMethod) {
         self::addRoute('PUT', $route, $serviceClassAndMethod);
     }
 
+    /**
+     * Agrega una ruta de la api junto a la clase y el metodo que van a administrar esa ruta
+     *
+     * @param string $route
+     * @param array $serviceClassAndMethod
+     * @return void
+     */
     public static function delete(string $route, array $serviceClassAndMethod) {
         self::addRoute('DELETE', $route, $serviceClassAndMethod);
     }
 
+    /**
+     * Agrega un array de elementos dentro del atributo estatico de clase $routes
+     *
+     * @param string $httpMethod
+     * @param string $route
+     * @param array $serviceClassAndMethod
+     * @return void
+     */
     private static function addRoute(string $httpMethod, string $route, array $serviceClassAndMethod) {
         $arrayKeys = array('service', 'method');
         array_push(self::$routes[strtoupper($httpMethod)], ['endPoint' => $route, 'serviceAndMethod' => array_combine($arrayKeys, $serviceClassAndMethod)]);
