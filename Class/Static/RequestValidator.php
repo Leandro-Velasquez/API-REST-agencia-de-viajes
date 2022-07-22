5c9e025e54ad41a0a6bb4f146bf451a5148b2800
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-21 22:30:04 -0300

    Definimos el metodo estatico checkMethod de la clase RequestValidator

--- Class/Static/RequestValidator.php
+++ Class/Static/RequestValidator.php
@@ -4,11 +4,17 @@ namespace Class\Static;
 
 class RequestValidator {
 
-    public static function checkRoute() {
-
+    public static function checkRoute(string $method, string $route) {
+        
     }
 
-    public static function checkMethod() {
-
+    /**
+     * Comprueba si el metodo http enviado en la request se encuentra definido en el servidor
+     *
+     * @param string $method
+     * @return bool
+     */
+    public static function checkMethod(string $method) {
+        return in_array(strtoupper($method), Routes::getAvailableHttpMethods());
     }
 }
