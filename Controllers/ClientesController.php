96a16c8a0b2accc312dd635c004ccc6bbc423e44
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-24 18:38:57 -0300

    Creamos la carpeta Controllers, creamos el archivo ClientesController.php declaramos la clase ClientesController junto con los metodos getAllClients, getClientById y registerClient

---
+++ Controllers/ClientesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controllers;
+
+use Class\Static\Response;
+
+class ClientesController {
+
+    public function getAllClients() {
+
+    }
+
+    public function getClientById() {
+
+    }
+
+    public function registerClient() {
+
+    }
+}
+?>
