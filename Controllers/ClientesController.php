0668ce2a5497fb5ed9cfd6b26dab3cac0eb1bd74
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-24 21:18:49 -0300

    Definimos el metodo getAllClients de la clase ClientesController

--- Controllers/ClientesController.php
+++ Controllers/ClientesController.php
@@ -2,12 +2,15 @@
 
 namespace Controllers;
 
+use Class\Static\JsonConverter;
 use Class\Static\Response;
+use Repository\ClientesRepository;
 
 class ClientesController {
 
     public function getAllClients() {
-
+        $r = new Response(array('Content-Type:application/json'), 200, JsonConverter::convertToJson(ClientesRepository::getAll()));
+        return $r;
     }
 
     public function getClientById() {
