d3c837fad17e4d66dd98fed503523efbed76f913
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-24 21:16:52 -0300

    Definimos el metodo send junto a los metodos getters y setters de la clase Response

--- Class/Static/Response.php
+++ Class/Static/Response.php
@@ -4,11 +4,11 @@ namespace Class\Static;
 
 class Response {
     
-    private $headers;
-    private $statusCode;
+    private array $headers;
+    private int $statusCode;
     private $body;
 
-    public function __construct(array $headers, $statusCode, string $body)
+    public function __construct(array $headers, int $statusCode, $body)
     {
         $this->headers = $headers;
         $this->statusCode = $statusCode;
@@ -16,7 +16,11 @@ class Response {
     }
 
     public function send() {
-
+        foreach($this->headers as $header) {
+            header($header);
+        }
+        http_response_code($this->statusCode);
+        echo $this->body;
     }
 
 
@@ -24,27 +28,27 @@ class Response {
     //Metodos getters y setters
     
     public function getHeaders() {
-
+        return $this->headers;
     }
 
     public function getStatusCode() {
-        
+        return $this->statusCode;
     }
 
     public function getBody() {
-        
+        return $this->body;
     }
 
-    public function setHeaders() {
-
+    public function setHeaders(array $headers) {
+        $this->headers = $headers;
     }
 
-    public function setStatusCode() {
-        
+    public function setStatusCode(int $statusCode) {
+        $this->statusCode = $statusCode;
     }
 
-    public function setBody() {
-        
+    public function setBody($body) {
+        $this->body = $body;
     }
 }
 ?>
