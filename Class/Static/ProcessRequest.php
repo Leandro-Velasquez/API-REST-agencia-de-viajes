4ec9c29344a4f05c7cc8929351a8e5e42c54d2cc
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-07-24 21:09:34 -0300

    Definimos los metodos estaticos process y validate de la clase ProcessRequest

--- Class/Static/ProcessRequest.php
+++ Class/Static/ProcessRequest.php
@@ -2,10 +2,32 @@
 
 namespace Class\Static;
 
+use InvalidArgumentException;
+
 class ProcessRequest {
 
-    public static function process() {
-        
+    public static function process(Request $request) {
+        if(self::validate($request)) {
+            extract(Routes::getRouteData($request->methodHttp, $request->uri));
+            $obj = new $controller;
+            $response = call_user_func_array(array($obj, $method), array());
+            return $response;
+        }
+    }
+
+    public static function validate(Request $request) {
+        if(RequestValidator::checkMethodHttp($request->methodHttp)) {
+            if(RequestValidator::checkRoute($request->methodHttp, $request->uri)) {
+                return true;
+            }
+            else {
+                StatusCode::setStatusCode(501);
+                throw new InvalidArgumentException('La ruta no se encuentra definida en la api');
+            }
+        }else {
+            StatusCode::setStatusCode(501);
+            throw new InvalidArgumentException('El metodo no esta definido en la api');
+        }
     }
 }
 ?>
