3aca7df9d3e7074b35a9638137463a3cbf8f259e
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-08-04 15:59:19 -0300

    Definimos los metodos estaticos containsVariables y getVariableValue de la clase ProcessRequest

--- Class/Static/ProcessRequest.php
+++ Class/Static/ProcessRequest.php
@@ -8,14 +8,15 @@ class ProcessRequest {
 
     public static function process(Request $request) {
         if(self::validate($request)) {
-            extract(Routes::getRouteData($request->methodHttp, $request->uri));
+            /*extract(Routes::getRouteData($request->methodHttp, $request->uri));
             $obj = new $controller;
             $response = call_user_func_array(array($obj, $method), array($request));
-            return $response;
+            return $response;*/
+
         }
     }
 
-    public static function validate(Request $request) {
+    private static function validate(Request $request) {
         if(RequestValidator::checkMethodHttp($request->methodHttp)) {
             if(RequestValidator::checkRoute($request->methodHttp, $request->uri)) {
                 return true;
@@ -27,6 +28,43 @@ class ProcessRequest {
         }else {
             StatusCode::setStatusCode(501);
             throw new InvalidArgumentException('El metodo no esta definido en la api');
+        }
+    }
+
+
+
+
+
+
+    /**
+     * Comprobamos si la url solicitada por la request contiene valores variables
+     *
+     * @param Request $request
+     * @return bool
+     */
+    private static function containsVariables(Request $request):bool {
+        $patternVariable = '/' . '[a-z]+\/((\d+)\/?|[a-z]+\/(\d+)\/?)$' . '/';
+        if(preg_match($patternVariable, $request->uri)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    /**
+     * Obtenemos el valor de la variable ubicada en la url pasada en la request
+     *
+     * @param Request $request
+     * @return void
+     */
+    private static function getVariableValue(Request $request) {
+        $patternVariable = '/' . '^[a-z]+\/(\d+)\/?$|^[a-z]+\/[a-z]+\/(\d+)\/?$' . '/';
+        if(preg_match($patternVariable, $request->uri, $match)) {
+            foreach($match as $x) {
+                if(is_numeric($x)) {
+                    return $x;
+                }
+            }
         }
     }
 }
